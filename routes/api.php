3c43532729fbe475a4eddb12c9e36c8a5236e92d
alainkatayi <alainkatayi01@gmail.com>
2025-04-29 15:01:55 +0100

    Creation des route d'authentification et implementation des differentes methode d'authentification

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register', [authController::class, 'register']);
+Route::post('/login', [authController::class, 'login']);
+Route::post('/logout', [authController::class, 'logout']) ->middleware('auth:sanctum');
