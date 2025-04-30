115cbb3765000d041b68ed1104f8c24794f8a17c
alainkatayi <alainkatayi01@gmail.com>
2025-04-30 09:46:12 +0100

    creation du useresource et du taskresource

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'is_completed' => $this->is_completed,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'priority' => $this->priority,
+            'user_id' => $this->user_id,
+  
+        ] ;
+    }
+}
